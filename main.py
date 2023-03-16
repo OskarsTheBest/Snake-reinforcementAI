88e1b3ee72944d1e400055a9d01e75a050174631
Oskars <122702900+OskarsTheBest@users.noreply.github.com>
2023-03-16 22:17:34 +0200

    Really simple Snake and Fruit rendering in one coloured background

--- main.py
+++ main.py
@@ -1,11 +1,45 @@
-import pygame, sys
+import pygame, sys, random
+from pygame.math import Vector2
 
+class Snake:
+    def __init__(self):
+        self.body = [Vector2(5,10),Vector2(6,10),Vector2(7,10)]
+        
+    def draw_snake(self):
+        for block in self.body:
+            x_pos = int(block.x * cell_size)
+            y_pos = int(block.y * cell_size)
+            block_rect = pygame.Rect(int(block.x * cell_size), int(block.y * cell_size), cell_size, cell_size)
+            pygame.draw.rect(screen,(60,113,54), block_rect)
+
+
+
+class Fruit:
+    def __init__(self):
+        self.x = random.randint(0, cell_number - 1)
+        self.y = random.randint(0, cell_number - 1)
+        self.pos = Vector2(self.x,self.y)
+        
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(int(self.pos.x * cell_size), int(self.pos.y * cell_size), cell_size, cell_size)
+        pygame.draw.rect(screen, (180, 41, 69) , fruit_rect)
+        
 pygame.init()
-screen = pygame.display.set_mode((400,500))
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
+clock = pygame.time.Clock()
+
+fruit = Fruit()
+snake = Snake()
 
 while True: 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    screen.fill((60,179,113))
+    fruit.draw_fruit()
+    snake.draw_snake()
     pygame.display.update()
+    clock.tick(120)
