3c5ca6f87f4faa39a59304df084334a583cd3497
Oskars <122702900+OskarsTheBest@users.noreply.github.com>
2023-03-18 15:12:57 +0200

    added grass snake graphics raspberry cant spawn inside you scoreboard

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pygame, sys, random
 from pygame.math import Vector2
 
+
 class Snake:
     def __init__(self):
         self.body = [Vector2(5,10),Vector2(4,10),Vector2(3,10)]
-        self.direction = Vector2(1,0)
+        self.direction = Vector2(0,0)
         self.new_block = False
         
         self.body_bl = pygame.image.load("static/body_bottomleft.png").convert_alpha()
@@ -25,19 +26,36 @@ class Snake:
         self.tail_right = pygame.image.load("static/tail_right.png").convert_alpha()
         self.tail_up = pygame.image.load("static/tail_up.png").convert_alpha()
         
+        self.eat_sound = pygame.mixer.Sound('Sound/crunch.wav')
         
     def draw_snake(self):
         self.update_head_graphics()
-        
+        self.update_tail_graphics()
         for index,block in enumerate(self.body):
             x_pos = int(block.x * cell_size)
             y_pos = int(block.y * cell_size)
             block_rect = pygame.Rect(x_pos,y_pos,cell_size,cell_size)
             
             if index == 0:
-                screen.blit(self.head_right,block_rect)
+                screen.blit(self.head,block_rect)
+            elif index == len(self.body) - 1:
+                screen.blit(self.tail, block_rect)
             else:
-                pygame.draw.rect(screen,(60,179,63), block_rect)
+                previous_block = self.body[index + 1] - block
+                next_block = self.body[index - 1] - block
+                if previous_block.x == next_block.x:
+                    screen.blit(self.body_vertical, block_rect)
+                elif previous_block.y == next_block.y:
+                    screen.blit(self.body_horizontal, block_rect)
+                else:
+                    if previous_block.x == -1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == -1:
+                        screen.blit(self.body_tl, block_rect)
+                    elif previous_block.x == -1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == -1:
+                        screen.blit(self.body_bl, block_rect)
+                    elif previous_block.x == 1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == 1:
+                        screen.blit(self.body_br, block_rect)
+                    elif previous_block.x == 1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == 1:
+                        screen.blit(self.body_tr, block_rect)
 
     def update_head_graphics(self):
         head_relation = self.body[1] - self.body[0]
@@ -46,7 +64,12 @@ class Snake:
         elif head_relation == Vector2(0,1): self.head = self.head_up
         elif head_relation == Vector2(0,-1): self.head = self.head_down
 
-
+    def update_tail_graphics(self):
+        tail_relation = self.body[-2] - self.body[-1]
+        if tail_relation == Vector2(1,0): self.tail = self.tail_left
+        elif tail_relation == Vector2(-1,0): self.tail = self.tail_right
+        elif tail_relation == Vector2(0,1): self.tail = self.tail_up
+        elif tail_relation == Vector2(0,-1): self.tail = self.tail_down
             
             
 
@@ -63,6 +86,14 @@ class Snake:
 
     def add_block(self):
         self.new_block = True
+        
+    def play_crunch_sound(self):
+        self.eat_sound.set_volume(1.0)
+        self.eat_sound.play()    
+
+    def reset(self):
+        self.body = [Vector2(5,10), Vector2(4,10), Vector2(3,10)]
+        self.direction = Vector2(0,0)
         
 class Fruit:
     def __init__(self):
@@ -83,6 +114,7 @@ class Main:
     def __init__(self):
         self.snake = Snake()
         self.fruit = Fruit()
+
         
     def update(self):
         self.snake.move_snake()
@@ -90,13 +122,20 @@ class Main:
         self.check_fail()
         
     def draw_elements(self):
+        self.draw_grass()
         self.fruit.draw_fruit()
         self.snake.draw_snake()
+        self.draw_score()
 
     def check_collision(self):
         if self.fruit.pos == self.snake.body[0]:
             self.fruit.randomize()
             self.snake.add_block()
+            self.snake.play_crunch_sound()
+            
+        for block in self.snake.body[1:]:
+            if block == self.fruit.pos:
+                self.fruit.randomize()
             
     def check_fail(self):
         if not 0 <= self.snake.body[0].x < cell_number:
@@ -110,15 +149,47 @@ class Main:
                 self.game_over()
             
     def game_over(self):
-        pygame.quit()
-        sys.exit()
+        self.snake.reset()
         
+        
+    def draw_grass(self):
+        grass_color = (148, 216, 81)
+        for row in range(cell_number):
+            if row % 2 == 0:
+                for col in range(cell_number):
+                    if col % 2 == 0:
+                        grass_rect = pygame.Rect(col * cell_size , row * cell_size , cell_size, cell_size)
+                        pygame.draw.rect(screen, grass_color, grass_rect)
+            else:
+                for col in range(cell_number):
+                    if col % 2 != 0:
+                        grass_rect = pygame.Rect(col * cell_size, row * cell_size, cell_size, cell_size)
+                        pygame.draw.rect(screen, grass_color, grass_rect)
+        
+    def draw_score(self):
+        score_text = str(len(self.snake.body) - 3)
+        score_surface = game_font.render(score_text, True, (13, 11, 58))
+        score_x = int(cell_size * cell_number - 60)
+        score_y = int(cell_size * cell_number - 40)
+        score_rect = score_surface.get_rect(center = (score_x, score_y))
+        raspberry_rect= raspberry.get_rect(midright = (score_rect.left, score_rect.centery))
+        bg_rect = pygame.Rect(raspberry_rect.left, raspberry_rect.top, raspberry_rect.width + score_rect.width + 6,raspberry_rect.height)
+        
+        pygame.draw.rect(screen, (167,209,61), bg_rect)
+        screen.blit(score_surface, score_rect)
+        screen.blit(score_image, raspberry_rect)
+        pygame.draw.rect(screen, (13,11,58), bg_rect,2)
+            
+pygame.mixer.pre_init(44100,-16,2,512)            
 pygame.init()
 cell_size = 40
 cell_number = 20
 screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
 clock = pygame.time.Clock()
-raspberry = pygame.image.load("static/Raspberry2.png").convert_alpha()
+score_image = pygame.image.load("static/RealRaspberry.png").convert_alpha()
+raspberry = pygame.image.load("static/Raspberry-.png").convert_alpha()
+game_font = pygame.font.Font("font/04B_30__.ttf", 25)
+
 
 
 SCREEN_UPDATE = pygame.USEREVENT
@@ -146,7 +217,8 @@ while True:
             if event.key == pygame.K_RIGHT:
                 if main_game.snake.direction.x != -1:
                     main_game.snake.direction = Vector2(1, 0)
-    screen.fill((60,179,113))
+
+    screen.fill((166,216,81))
     main_game.draw_elements()
     pygame.display.update()
-    clock.tick(120)
+    clock.tick(200)
